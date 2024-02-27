90b0a9f682b8312c18ac495d68be011dcf0af8b5
Coll76 <collins.kubu@gmail.com>
2024-02-27 12:21:36 -0800

    0-simply_match_school.rb

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+#regular expression must match School
+#Ruby script that accepts one argument and pass it to a regular expression matching method
+puts ARGV[0].scan(/School/).join
