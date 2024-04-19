700545ec699a0f26b34f076bb297020d8c79df36
Coll76 <collins.kubu@gmail.com>
2024-04-19 15:51:04 -0700

    0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#create a file
+# Define a file resource to create the file in /tmp
+file { '/tmp/school':
+ensure => present,
+content => "I love Puppet",
+mode => '0744',
+owner => 'www-data',
+group => 'www-data'
+}
